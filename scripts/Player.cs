54d5ac89965028acb682bc5130ab66f92a3b206c
onnik <onni.kivinen@gmail.com>
2025-05-14 16:47:55 +0300

    fixed player-model animation sprites and added diagonal movement to player script

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -5,7 +5,8 @@ public partial class Player : CharacterBody2D
 {
     public const float Speed = 200.0f;
 
-    private string playerFacing = "south";
+    // initial direction the player model is facing
+    private string playerFacing = "down";
     private AnimatedSprite2D animatedSprite;
 
     public override void _Ready()
@@ -16,36 +17,44 @@ public partial class Player : CharacterBody2D
     public override void _PhysicsProcess(double delta)
     {
         Vector2 velocity = Vector2.Zero;
-
         Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 
-
-            if (direction == Vector2.Right)
+        if (direction == Vector2.Zero)
+        {
+            animatedSprite.Play($"idle-{playerFacing}");
+        }
+        else
+        {
+            if (direction.X > 0)
             {
-                animatedSprite.Play("animation_walk_east");
-                playerFacing = "east";
+                animatedSprite.Play("walk-right");
+                playerFacing = "right";
             }
-            else if (direction == Vector2.Left)
+            else if (direction.X < 0)
             {
-                animatedSprite.Play("animation_walk_west");
-                playerFacing = "west";
-            }
-            else if (direction == Vector2.Up)
-            {
-                animatedSprite.Play("animation_walk_north");
-                playerFacing = "north";
-            }
-            else if (direction == Vector2.Down)
-            {
-                animatedSprite.Play("animation_walk_south");
-                playerFacing = "south";
-            }
-            else
-            {
-                animatedSprite.Play($"animation_idle_{playerFacing}");
+                animatedSprite.Play("walk-left");
+                playerFacing = "left";
             }
 
-        velocity = direction * Speed;
+            if (direction.Y < 0)
+            {
+                if (direction.X == 0)
+                {
+                    animatedSprite.Play("walk-up");
+                    playerFacing = "up";
+                }
+            }
+            else if (direction.Y > 0)
+            {
+                if (direction.X == 0)
+                {
+                    animatedSprite.Play("walk-down");
+                    playerFacing = "down";
+                }
+            }
+        }
+
+        velocity = direction.Normalized() * Speed;
         Velocity = velocity;
         MoveAndSlide();
     }
