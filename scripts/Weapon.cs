c32f3a088a5c27d68eb94b8a15f6b59e3f17b395
onnik <onni.kivinen@gmail.com>
2025-05-20 17:27:32 +0300

    reformatoitu tiedostojen nimiä ja sijainteja, alustettu gunsmoke sprite ja bullet rifle modelille.

--- scripts/Weapon.cs
+++ scripts/Weapon.cs
@@ -8,6 +8,8 @@ public partial class Weapon : Node2D
 	private Player playerInRange;
 	private float mousePosition;
 	public PackedScene BulletScene = GD.Load<PackedScene>("res://scenes/bullet.tscn");
+	private PackedScene gunSmokeScene = GD.Load<PackedScene>("res://scenes/gunsmoke.tscn");
+	private Node2D gunSmokeInstance;
 
 	public override void _Ready()
 	{
@@ -16,6 +18,11 @@ public partial class Weapon : Node2D
 		var area = GetNode<Area2D>("Area2D");
 		area.BodyEntered += OnBodyEntered;
 		area.BodyExited += OnBodyExited;
+
+		// Instance the gunsmoke and add as child, but keep hidden
+		gunSmokeInstance = gunSmokeScene.Instantiate<Node2D>();
+		AddChild(gunSmokeInstance);
+		gunSmokeInstance.Visible = false;
 	}
 
 	private void OnBodyEntered(Node body)
@@ -57,6 +64,7 @@ public partial class Weapon : Node2D
 
 		mousePosition = (RotationDegrees % 360 + 360) % 360;
 
+
 		if (mousePosition > 90 && mousePosition < 270)
 			Scale = new Vector2(Scale.X, -1);
 		else
@@ -70,6 +78,11 @@ public partial class Weapon : Node2D
 			bullet.Rotation = Rotation;
 			shootAudio.Play();
 
+			gunSmokeInstance.GlobalPosition = muzzle.GlobalPosition;
+			gunSmokeInstance.Rotation = Mathf.Sin(Mathf.DegToRad(mousePosition));
+			gunSmokeInstance.Visible = true;
+
+			GetTree().CreateTimer(0.15f).Timeout += () => gunSmokeInstance.Visible = false;
 		}
 	}
 }
